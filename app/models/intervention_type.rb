8fa85caa5b413b8739d1742adf63b56575368718
dermatobia <webblewebble@gmail.com>
2014-06-14 23:44:13 -0400

    modify model to rename service_category to intervention_type

--- app/models/intervention_type.rb
+++ app/models/intervention_type.rb
@@ -1,4 +1,4 @@
-class ServiceCategory < ActiveRecord::Base
+class InterventionType < ActiveRecord::Base
 	has_and_belongs_to_many :service_providers
 	validates :name, presence: :true, uniqueness: :true
 end
