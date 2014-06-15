d74fa75566d4e343cb57b5fef5088f8e9ad8eec7
dermatobia <webblewebble@gmail.com>
2014-06-15 00:35:46 -0400

    corrected table name

--- db/migrate/20140610025246_create_service_provider_intervention_type_join_table.rb
+++ db/migrate/20140610025246_create_service_provider_intervention_type_join_table.rb
@@ -1,6 +1,6 @@
 class CreateServiceProviderInterventionTypeJoinTable < ActiveRecord::Migration
   def change
-  	create_table :intervention_types_providers do |t|
+  	create_table :intervention_types_service_providers do |t|
   		t.belongs_to :service_provider 
   		t.belongs_to :intervention_type
   	end
