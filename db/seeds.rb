e32e2b1c37e0fb866fec0ae146f5c35ef59558b3
dermatobia <webblewebble@gmail.com>
2014-06-15 01:29:09 -0400

    comment out img_url because it is giving error when seed file is unable to retrieve remote image

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ end
 #-------- SERVICE PROVIDERS ---------
 prov1 = ServiceProvider.create( name: 'Hidden Gems Archery', 
 												mission: 'to bring archery to the masses', 
-												remote_image_url:'http://www.hiddengemsarchery.com/wp-content/uploads/2013/02/cropped-Screen-Shot-2013-02-12-at-11.45.43-AM-e1360885247611.png',
+												# remote_image_url:'http://www.hiddengemsarchery.com/wp-content/uploads/2013/02/cropped-Screen-Shot-2013-02-12-at-11.45.43-AM-e1360885247611.png',
 												website: 'http://www.hiddengemsarchery.com/', 
 												contact_person: 'q', 
 												contact_email:'q@hga@aol.com', 
@@ -29,8 +29,6 @@ prov1 = ServiceProvider.create( name: 'Hidden Gems Archery',
 												work_phone:'254-247-4477', 
 												fax:'', 
 												other:'')
-
-
 
 prov2 = ServiceProvider.create( name: 'Cardinal McCloskey' , 
 												mission: 'Our At-Risk Services’ mission is to promote a safe and nurturing environment for at-risk children and youth and the goal is to keep children safe, prevent abuse and strengthen biological families.', 
@@ -47,7 +45,8 @@ prov2 = ServiceProvider.create( name: 'Cardinal McCloskey' ,
 												fax:'', 
 												other:'')
 
-#--- CREATE SAMPLE RELATIONSHIP BETWEEN SERVICE PROVIDER AND INTERVENTION TYPE ----
+
+# --- CREATE SAMPLE RELATIONSHIP BETWEEN SERVICE PROVIDER AND INTERVENTION TYPE ----
 type1 = InterventionType.all[1]
 type4 = InterventionType.all[4]
 type13 = InterventionType.all[13]
