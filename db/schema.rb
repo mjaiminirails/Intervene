380a1112d899c2d46139255d02b157a06cf0d507
Tasha25 <greennatasha@aol.com>
2014-06-16 01:17:10 -0400

    primary key removed from intervention_types_service_providers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610025246) do
+ActiveRecord::Schema.define(version: 20140616050113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20140610025246) do
     t.datetime "updated_at"
   end
 
-  create_table "intervention_types_service_providers", force: true do |t|
+  create_table "intervention_types_service_providers", id: false, force: true do |t|
     t.integer "service_provider_id"
     t.integer "intervention_type_id"
   end
