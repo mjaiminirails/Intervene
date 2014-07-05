e90bcd5e6ff14dee325d87121b96d6ffdd9240ec
dermatobia <webblewebble@gmail.com>
2014-07-05 11:49:27 -0400

    modify dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,51 +3,35 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+    
     columns do
       column do
-        panel "Service Categories" do
+        panel "Unpublished Service Providers" do
           ul do
-            ServiceProvider.all.map do |prov|
+            ServiceProvider.where(published: false).each do |prov|
               li link_to(prov.name, admin_service_provider_path(prov))
             end
-          end
-        end
-      end
+          end  # end of ul
+        end  # end of panel
+      end  # end of column
+
+      column do 
+        panel "Published Service Providers" do
+          ul do 
+            ServiceProvider.where(published: true).each do |prov|
+              li link_to(prov.name, admin_service_provider_path(prov))
+            end
+          end  # end of ul
+        end  # end of panel
+      end  # end of column
     end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
 
-
-    # columns do
-    #   column do
-    #     panel "Service Categories" do
-    #       ul do
-    #         ServiceCategory.all.map do |cat|
-    #           li link_to(cat.name, admin_service_category_path(cat))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
